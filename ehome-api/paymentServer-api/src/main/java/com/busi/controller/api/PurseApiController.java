bfdfed3866008bc35db5c6c3e1ddbcafae44ddc7
ËïÌì½Ü <552440192@qq.com>
2018-08-27 19:19:07 +0800

    1、添加部分兑换逻辑

--- ehome-api/paymentServer-api/src/main/java/com/busi/controller/api/PurseApiController.java
+++ ehome-api/paymentServer-api/src/main/java/com/busi/controller/api/PurseApiController.java
@@ -4,6 +4,7 @@ import com.busi.entity.ReturnData;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+
 /**
  * 钱包相关接口
  * author：SunTianJie
@@ -27,5 +28,15 @@ public interface PurseApiController {
      */
     @GetMapping("findHomePointInfo/{myId}/{userId}")
     ReturnData findHomePointInfo(@PathVariable long myId,@PathVariable long userId);
+
+    /**
+     * 钱包兑换接口
+     * @param userId       当前用户ID
+     * @param exChangeType 兑换类型 1家币兑换家点（1:100） 2人民币兑换家币（1:1）
+     * @param money        当exChangeType==1时人民币兑换家币 当exChangeType==2时家币兑换家点
+     * @return
+     */
+    @GetMapping("exChange/{userId}/{exChangeType}/{money}")
+    ReturnData exChange(@PathVariable long userId,@PathVariable int exChangeType,@PathVariable long money);
 
 }
--- ehome-paymentServer/src/main/java/com/busi/controller/api/PurseController.java
+++ ehome-paymentServer/src/main/java/com/busi/controller/api/PurseController.java
@@ -3,8 +3,10 @@ package com.busi.controller.api;
 import com.alibaba.fastjson.JSONObject;
 import com.busi.controller.BaseController;
 import com.busi.entity.Purse;
+import com.busi.entity.PursePayPassword;
 import com.busi.entity.ReturnData;
 import com.busi.service.PurseInfoService;
+import com.busi.service.PursePayPasswordService;
 import com.busi.utils.CommonUtils;
 import com.busi.utils.Constants;
 import com.busi.utils.RedisUtils;
@@ -28,6 +30,9 @@ public class PurseController extends BaseController implements PurseApiControlle
 
     @Autowired
     private PurseInfoService purseInfoService;
+
+    @Autowired
+    private PursePayPasswordService pursePayPasswordService;
 
     /***
      * 查询用户钱包信息
@@ -113,5 +118,68 @@ public class PurseController extends BaseController implements PurseApiControlle
         map.put("myHomePoint",Long.parseLong(myIdPurseMap.get("homePoint").toString()));
         map.put("inviteeHomePoint",Long.parseLong(userIdPurseMap.get("homePoint").toString()));
         return returnData(StatusCode.CODE_SUCCESS.CODE_VALUE,"success",map);
+    }
+
+
+    /**
+     * 钱包兑换接口
+     * @param userId       当前用户ID
+     * @param exChangeType 兑换类型 1家币兑换家点（1:100） 2人民币兑换家币（1:1）
+     * @param money        当exChangeType==1时人民币兑换家币 当exChangeType==2时家币兑换家点
+     * @return
+     */
+    @Override
+    public ReturnData exChange(@PathVariable long userId,@PathVariable int exChangeType,@PathVariable long money) {
+        //验证参数
+        if(userId<=0){
+            return returnData(StatusCode.CODE_PARAMETER_ERROR.CODE_VALUE,"userId参数有误",new JSONObject());
+        }
+        if(exChangeType<1||exChangeType>2){
+            return returnData(StatusCode.CODE_PARAMETER_ERROR.CODE_VALUE,"exChangeType参数有误",new JSONObject());
+        }
+        if(money<=0){
+            return returnData(StatusCode.CODE_PARAMETER_ERROR.CODE_VALUE,"money参数有误",new JSONObject());
+        }
+        //验证身份
+        if(CommonUtils.getMyId()!=userId){
+            return returnData(StatusCode.CODE_PARAMETER_ERROR.CODE_VALUE,"userId参数有误,无权限进行此操作",new JSONObject());
+        }
+        //检测账户信息
+        Map<String,Object> purseMap = redisUtils.hmget(Constants.REDIS_KEY_PAYMENT_PURSEINFO+userId );
+        if(purseMap==null||purseMap.size()<=0){
+            Purse purse = null;
+            //缓存中没有用户对象信息 查询数据库
+            purse = purseInfoService.findPurseInfo(userId);
+            if(purse==null){
+                return returnData(StatusCode.CODE_PURSE_NOT_ENOUGH_ERROR.CODE_VALUE,"您账户余额不足，无法进行兑换操作",new JSONObject());
+            }
+            purseMap = CommonUtils.objectToMap(purse);
+        }
+        //检测是否设置过支付密码
+        Map<String,Object> payPasswordMap = redisUtils.hmget(Constants.REDIS_KEY_PAYMENT_PAYPASSWORD+userId );
+        if(payPasswordMap==null||payPasswordMap.size()<=0){
+            PursePayPassword pursePayPassword = null;
+            //缓存中没有用户对象信息 查询数据库
+            pursePayPassword = pursePayPasswordService.findPursePayPassword(userId);
+            if(pursePayPassword==null){
+                return returnData(StatusCode.CODE_PAYPASSWORD_IS_NOT_EXIST_ERROR.CODE_VALUE,"您当前账户尚未设置过支付密码，无法进行兑换操作",new JSONObject());
+            }
+            payPasswordMap = CommonUtils.objectToMap(pursePayPassword);
+        }
+        //开始兑换
+        if(exChangeType==1){//人民币兑换家币
+            double spareMoney = Double.parseDouble(purseMap.get("spareMoney").toString());
+            if(spareMoney>=money){
+                //生成订单
+
+            }
+        }else{//家币兑换家点
+            long homePoint = Long.parseLong(purseMap.get("homePoint").toString());
+            if(homePoint>=money){
+                //生成订单
+
+            }
+        }
+        return null;
     }
 }
