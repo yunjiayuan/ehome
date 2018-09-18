37b9dee9ec0cb02863e03972426600296962590d
suntj <552440192@qq.com>
2018-09-18 16:19:22 +0800

    1、完善短信接口

--- ehome-message/src/main/java/com/busi/service/PhoneService.java
+++ ehome-message/src/main/java/com/busi/service/PhoneService.java
@@ -28,6 +28,16 @@ public class PhoneService implements MessageAdapter {
             content = "云家园提醒您，您的手机注册验证码为：" + phoneCode + ",请您在10分钟之内使用，感谢您对云家园的关注与支持!";
         }else if(phoneType==1){//支付密码找回 验证手机号
             content = "云家园提醒您，您的支付密码找回验证码为：" + phoneCode + ",请您在10分钟之内使用，感谢您对云家园的关注与支持!";
+        }else if(phoneType==2){//安全中心绑定手机验证码
+            content = "云家园提醒您，安全中心绑定手机验证码为：" + phoneCode + ",请您在10分钟之内使用，感谢您对云家园的关注与支持!";
+        }else if(phoneType==3){//安全中心解绑手机验证码
+            content = "云家园提醒您，安全中心解绑手机验证码为：" + phoneCode + ",请您在10分钟之内使用，感谢您对云家园的关注与支持!";
+        }else if(phoneType==4){//手机短信找回登录密码验证码
+            content = "云家园提醒您，找回登录密码验证码为：" + phoneCode + ",请您在10分钟之内使用，感谢您对云家园的关注与支持!";
+        }else if(phoneType==5) {//手机短信修改密码验证码
+            content = "云家园提醒您，修改密码验证码为：" + phoneCode + ",请您在10分钟之内使用，感谢您对云家园的关注与支持!";
+        }else if(phoneType==6){//短信邀请新用户注册
+                content = "云家园提醒您，您的朋友邀请您成为云家园新用户，邀请码为：" + phoneCode + ",最新云家园官方下载地址为：http://ephone.lichengwang.com/downLoad/index.html?shareCode=\"+code+\"，感谢您对历程网的关注与支持!";
         }else{
             //预留
         }
