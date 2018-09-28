661cdef9687e7c401fe041cdd486fe73a816d2d5
suntj <552440192@qq.com>
2018-09-28 09:52:52 +0800

    1、添加修改密码临时key

--- ehome-common/src/main/java/com/busi/utils/Constants.java
+++ ehome-common/src/main/java/com/busi/utils/Constants.java
@@ -168,6 +168,7 @@ public class Constants {
     public static final String REDIS_KEY_USER_ACCOUNT_SECURITY_FINDPASSWORD_CODE = "user_account_security_findPassword_code_";//手机短信找回登录密码验证码 短信验证码对应的key
     public static final String REDIS_KEY_USER_ACCOUNT_SECURITY_CHANGEPASSWORD_CODE = "user_account_security_changePassword_code_";//手机短信修改密码验证码 短信验证码对应的key
     public static final String REDIS_KEY_USER_ACCOUNT_SECURITY_INVITATION_CODE = "user_account_security_invitation_code_";//短信邀请新用户注册 短信验证码对应的key
+    public static final String REDIS_KEY_USER_CHANGE_PASSWORD_KEY = "user_account_change_password_key_";//修改密码临时key
 
     //短信次数验证key
     public static final String REDIS_KEY_ACCOUNT_DAY_TOTAL = "sendMsg_account_day_total";//同一账号每天最多发送短信次数限制KEY
