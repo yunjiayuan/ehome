bea8dcc52d1f095c5d5342fac1885d6989a179cd
赵家杰 <746331520@qq.com>
2018-10-25 16:06:29 +0800

    1、新增

---
+++ ehome-api/otherServer-api/src/main/java/com/busi/entity/HomeAlbumPwd.java
@@ -0,0 +1,21 @@
+package com.busi.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @program: 存储室相册密码
+ * @author: ZHaoJiaJie
+ * @create: 2018-10-18 10:58:47
+ */
+@Setter
+@Getter
+public class HomeAlbumPwd {
+
+    private long id;
+
+    private String password;//用户设置的一级密码
+
+    private String status;//随机生成的状态码
+
+}
