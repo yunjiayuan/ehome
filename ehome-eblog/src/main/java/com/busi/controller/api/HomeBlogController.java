99b1a228615d4a88404f62c0f1767382343eb709
suntj <552440192@qq.com>
2018-11-08 16:23:20 +0800

    1、添加生活圈转发、点赞新增消息功能

--- ehome-eblog/src/main/java/com/busi/controller/api/HomeBlogController.java
+++ ehome-eblog/src/main/java/com/busi/controller/api/HomeBlogController.java
@@ -186,6 +186,10 @@ public class HomeBlogController extends BaseController implements HomeBlogApiCon
         mqUtils.sendFootmarkMQ(homeBlog.getUserId(), t, homeBlog.getImgUrl(), null, null, homeBlog.getId()+"", 2);
         //添加任务
         mqUtils.sendTaskMQ(homeBlog.getUserId(), 1, 1);
+        //添加转发消息
+        if(homeBlog.getBlogType()==1){
+            mqUtils.addMessage(homeBlog.getUserId(),homeBlog.getOrigUserId(),homeBlog.getOrigBlogId(),0,homeBlog.getReprintContent(),3);
+        }
         return returnData(StatusCode.CODE_SUCCESS.CODE_VALUE,"success",new JSONObject());
     }
 
