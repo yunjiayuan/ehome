9615f292d20d756862b3cecb67e2dd72747366d1
赵家杰 <746331520@qq.com>
2018-12-20 10:50:19 +0800

    1、调整消息接口

--- ehome-eblog/src/main/java/com/busi/dao/HomeBlogCommentDao.java
+++ ehome-eblog/src/main/java/com/busi/dao/HomeBlogCommentDao.java
@@ -32,8 +32,8 @@ public interface HomeBlogCommentDao {
      * @param homeBlogMessage
      * @return
      */
-    @Insert("insert into homeBlogMessage(userId,blog,replayId,commentId,content,time,newsType,newsState,status,masterId) " +
-            "values (#{userId},#{blog},#{replayId},#{commentId},#{content},#{time},#{newsType},#{newsState},#{status},#{masterId})")
+    @Insert("insert into homeBlogMessage(userId,blog,replayId,commentId,content,time,newsType,newsState,status,masterId,origBlogId) " +
+            "values (#{userId},#{blog},#{replayId},#{commentId},#{content},#{time},#{newsType},#{newsState},#{status},#{masterId},#{origBlogId})")
     @Options(useGeneratedKeys = true)
     int addMessage(HomeBlogMessage homeBlogMessage);
 
