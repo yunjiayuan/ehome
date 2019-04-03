9fd37ed16c27bedf18f97096186cd25b72b572ec
suntj <552440192@qq.com>
2019-04-03 16:37:06 +0800

    1、新增activity定时任务meavn模块

---
+++ ehome-quartz-activity/src/main/java/com/busi/QuartzActivityApplication.java
@@ -0,0 +1,21 @@
+package com.busi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * 其他应用服务定时任务系统主入口，如 喂鸟等
+ * author：SunTianJie
+ * create time：2018/7/23 10:15
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableScheduling
+public class QuartzActivityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QuartzActivityApplication.class,args);
+    }
+
+}
