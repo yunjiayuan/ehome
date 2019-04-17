391fd39a6c46d6f268fe86816a2abf4d5718f91e
suntj <552440192@qq.com>
2019-04-17 15:36:44 +0800

    1、新增家店系统-家店中心相关结构

---
+++ ehome-homeShop-shopCenter/src/main/java/com/busi/controller/api/ShopCenterController.java
@@ -0,0 +1,14 @@
+package com.busi.controller.api;
+
+import com.busi.controller.BaseController;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 店铺信息相关接口 如：创建店铺 修改店铺信息 更改店铺状态等
+ * author：SunTianJie
+ * create time：2019/4/17 15:31
+ */
+@RestController
+public class ShopCenterController extends BaseController implements ShopCenterApiController {
+
+}
