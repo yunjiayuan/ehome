780942987186d224269df0c09b1d568833590a3c
赵家杰 <746331520@qq.com>
2019-04-26 10:23:44 +0800

    1.小时工订单实体

--- ehome-api/otherServer-api/src/main/java/com/busi/entity/HourlyWorkerOrders.java
+++ ehome-api/otherServer-api/src/main/java/com/busi/entity/HourlyWorkerOrders.java
@@ -34,6 +34,9 @@ public class HourlyWorkerOrders {
     private Date addTime;            // 添加时间
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date paymentTime;            // 付款时间
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date orderTime;            // 接单时间
 
     private String serviceTime;                // 到达时间【显示用】
