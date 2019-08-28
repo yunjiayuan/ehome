37369cfdfb3a43212cdcb44ea91fc4f7bb546333
suntj <552440192@qq.com>
2019-08-28 16:58:52 +0800

    1、修改订座支付接口，实现加菜支付和普通支付

--- ehome-paymentServer/src/main/java/com/busi/service/KitchenBookedOrdersService.java
+++ ehome-paymentServer/src/main/java/com/busi/service/KitchenBookedOrdersService.java
@@ -42,14 +42,19 @@ public class KitchenBookedOrdersService extends BaseController implements PayBas
         //获取未支付的订单
         Map<String,Object> kitchenBookedOrderMap = redisUtils.hmget(Constants.REDIS_KEY_KITCHENBOOKEDORDERS+pay.getUserId()+"_"+pay.getOrderNumber() );
         if(kitchenBookedOrderMap==null||kitchenBookedOrderMap.size()<=0){
-            return returnData(StatusCode.CODE_PAY_OBJECT_NOT_EXIST_ERROR.CODE_VALUE,"由于您等待时间过久或网络延迟导致支付厨房订单失败，请重新下单",new JSONObject());
+            return returnData(StatusCode.CODE_PAY_OBJECT_NOT_EXIST_ERROR.CODE_VALUE,"由于您等待时间过久或网络延迟导致支付美食订座订单失败，请重新下单",new JSONObject());
         }
         KitchenBookedOrders kitchenBookedOrders = (KitchenBookedOrders)CommonUtils.mapToObject(kitchenBookedOrderMap,KitchenBookedOrders.class);
-        if(kitchenBookedOrders==null||kitchenBookedOrders.getOrdersType()!=0){
-            return returnData(StatusCode.CODE_PAY_OBJECT_NOT_EXIST_ERROR.CODE_VALUE,"由于您等待时间过久或网络延迟导致支付厨房订单失败，请重新下单!",new JSONObject());
+        if(kitchenBookedOrders==null||kitchenBookedOrders.getPaymentStatus()!=0){
+            return returnData(StatusCode.CODE_PAY_OBJECT_NOT_EXIST_ERROR.CODE_VALUE,"由于您等待时间过久或网络延迟导致支付美食订座订单失败，请重新下单!",new JSONObject());
         }
         //判断余额
-        double money = kitchenBookedOrders.getMoney();//将要花费的钱
+        double money = 0;//将要花费的钱
+        if(kitchenBookedOrders.getAddToFoodMoney()>0){//判断是否为加菜
+            money = kitchenBookedOrders.getAddToFoodMoney();//加菜
+        }else{
+            money = kitchenBookedOrders.getMoney();
+        }
         Purse purse = (Purse) CommonUtils.mapToObject(purseMap,Purse.class);
         if(purse==null){
             return returnData(StatusCode.CODE_PURSE_NOT_ENOUGH_ERROR.CODE_VALUE,"您账户余额不足，无法进行支付操作",new JSONObject());
@@ -60,8 +65,9 @@ public class KitchenBookedOrdersService extends BaseController implements PayBas
         }
         //更改状态 防止重复支付
         redisUtils.hset(Constants.REDIS_KEY_KITCHENBOOKEDORDERS+pay.getUserId()+"_"+pay.getOrderNumber(),"ordersType",1);
-        kitchenBookedOrders.setOrdersType(1);
+        kitchenBookedOrders.setPaymentStatus(1);
         kitchenBookedOrders.setPaymentTime(new Date());
+        kitchenBookedOrders.setAddToFoodMoney(0);//将会根据这个字段来来判断是否为加菜支付
         redisUtils.expire(Constants.REDIS_KEY_KITCHENBOOKEDORDERS + pay.getUserId()+"_"+pay.getOrderNumber(), 0);
         redisUtils.hmset(Constants.REDIS_KEY_KITCHENBOOKEDORDERS + pay.getUserId()+"_"+pay.getOrderNumber(), CommonUtils.objectToMap(kitchenBookedOrders), Constants.TIME_OUT_MINUTE_15*2);
         //开始扣款支付
