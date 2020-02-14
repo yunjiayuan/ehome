6ee6b341d6f8d858712f5af011dd11a3c8a81593
suntj <552440192@qq.com>
2020-02-14 14:57:51 +0800

    1、添加疫情数据实体

---
+++ ehome-api/otherServer-api/src/main/java/com/busi/entity/EpidemicSituationImage.java
@@ -0,0 +1,18 @@
+package com.busi.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 疫情图片实体对象
+ * author suntj
+ * Create time 2020/2/14 14:52
+ */
+@Setter
+@Getter
+public class EpidemicSituationImage {
+
+    private String imgUrl;//图片地址
+
+    private String title;//图片标题
+}
