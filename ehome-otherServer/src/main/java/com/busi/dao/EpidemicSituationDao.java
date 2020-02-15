b130706e89c913984bd0d14dca2771f2614ebb12
ZhaoJiaJie <tony.jj@foxmail.com>
2020-02-15 12:19:10 +0800

    1.新增疫情相关接口

---
+++ ehome-otherServer/src/main/java/com/busi/dao/EpidemicSituationDao.java
@@ -0,0 +1,40 @@
+package com.busi.dao;
+
+import com.busi.entity.EpidemicSituation;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * 疫情相关Dao
+ * author：zhaojiajie
+ * create time：2020-02-15 11:30:28
+ */
+@Mapper
+@Repository
+public interface EpidemicSituationDao {
+
+    /***
+     * 新增
+     * @param epidemicSituation
+     * @return
+     */
+    @Insert("insert into EpidemicSituation(modifyTime,createTime,imgUrl,dailyPic,summary,deleted,countRemark,currentConfirmedCount,confirmedCount,suspectedCount,curedCount,deadCount,seriousCount,remark1,remark2,remark3," +
+            "remark4,remark5,note1,note2,note3,generalRemark,abroadRemark,quanguoTrendCharts,hbFeiHbTrendCharts,listByArea,listByOther)" +
+            "values (#{modifyTime},#{createTime},#{imgUrl},#{dailyPic},#{summary},#{deleted},#{countRemark},#{currentConfirmedCount},#{confirmedCount},#{suspectedCount},#{curedCount},#{deadCount},#{seriousCount},#{remark1},#{remark2},#{remark3}" +
+            ",#{remark4},#{remark5},#{note1},#{note2},#{note3},#{generalRemark},#{abroadRemark},#{quanguoTrendCharts},#{hbFeiHbTrendCharts},#{listByArea},#{listByOther})")
+    @Options(useGeneratedKeys = true)
+    int add(EpidemicSituation epidemicSituation);
+
+    /***
+     * 查询列表
+     * @return
+     */
+    @Select("<script>" +
+            "select * from EpidemicSituation" +
+            " where 1=1" +
+            " ORDER BY id DESC LIMIT 1" +
+            "</script>")
+    List<EpidemicSituation> findList();
+}
---
+++ ehome-quartz-otherServer/src/main/java/com/busi/utils/EpidemicSituationUtils.java
@@ -0,0 +1,114 @@
+package com.busi.utils;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.busi.entity.EpidemicSituation;
+import com.busi.entity.EpidemicSituationImage;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/** 
+ * 疫情工具类
+ *
+ * @author SunTianJie 
+ *
+ * @version create time：2017-6-5 下午1:37:14 
+ * 
+ */
+@Slf4j
+public class EpidemicSituationUtils {
+	
+	/****
+	 * 调用第三方疫情接口主逻辑
+	 * @return
+	 */
+	public static EpidemicSituation getEpidemicSituation(){
+
+		String returnStr = null; // 返回结果定义
+		URL url = null;
+		HttpURLConnection httpURLConnection = null;
+		try {
+			url = new URL(Constants.EPIDEMIC_SITUATION_URL);
+			httpURLConnection = (HttpURLConnection) url.openConnection();
+			httpURLConnection.setRequestProperty("Accept-Charset", "utf-8");
+			httpURLConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+			httpURLConnection.setDoOutput(true);
+			httpURLConnection.setDoInput(true);
+			httpURLConnection.setRequestMethod("GET");
+			httpURLConnection.connect();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream(), "utf-8"));
+			StringBuffer buffer = new StringBuffer();
+			String line = "";
+			while ((line = reader.readLine()) != null) {
+				buffer.append(line);
+			}
+			reader.close();
+			returnStr = buffer.toString();
+			//解析json
+			JSONObject jsonObj = JSONObject.parseObject(returnStr);
+			if(jsonObj!=null){
+				int error_code = -1;
+				String reason = "";
+				error_code = jsonObj.getInteger("error");//状态码
+				if(error_code==0){//成功
+					JSONObject data = jsonObj.getJSONObject("data");
+					//获取数据概要对象
+					JSONObject statisticsJosnObject = data.getJSONObject("statistics");
+					EpidemicSituation statistics = JSON.toJavaObject(statisticsJosnObject,EpidemicSituation.class);
+					if(statistics!=null){
+						EpidemicSituationImage[] quanguoarray = statistics.getQuanguoTrendChart();
+						if(quanguoarray!=null){
+							String quanguoTrendCharts = "";
+							for (int i = 0; i <quanguoarray.length ; i++) {
+								String quanguoData = quanguoarray[i].getImgUrl()+","+quanguoarray[i].getTitle();
+								if(i!=quanguoarray.length-1){
+									quanguoTrendCharts  += quanguoData+";";
+								}else{
+									quanguoTrendCharts  += quanguoData;
+								}
+							}
+							statistics.setQuanguoTrendCharts(quanguoTrendCharts);
+						}
+						EpidemicSituationImage[] hbFeiHbTrendChart= statistics.getHbFeiHbTrendChart();
+						if(hbFeiHbTrendChart!=null){
+							String hbFeiHbTrendCharts = "";
+							for (int i = 0; i <hbFeiHbTrendChart.length ; i++) {
+								String hbFeiHbData = hbFeiHbTrendChart[i].getImgUrl()+","+hbFeiHbTrendChart[i].getTitle();
+								if(i!=hbFeiHbTrendChart.length-1){
+									hbFeiHbTrendCharts  += hbFeiHbData+";";
+								}else{
+									hbFeiHbTrendCharts  += hbFeiHbData;
+								}
+							}
+							statistics.setHbFeiHbTrendCharts(hbFeiHbTrendCharts);
+						}
+						statistics.setListByArea(data.getJSONArray("listByArea").toString());//国内各省市数据
+						statistics.setListByOther(data.getJSONArray("listByOther").toString());//国外数据
+						return statistics;
+					}
+				}else{//失败
+					//打印错误信息
+					log.info("第三方疫情平台状态码异常："+error_code);
+					return null;
+				}
+			}else{
+				//打印错误信息
+				log.info("第三方疫情平台响应数据格式有误！");
+				return null;
+			}
+		} catch (Exception e) {
+			log.info("解析第三方疫情平台数据异常！");
+			e.printStackTrace();
+			return null;
+		} finally {
+			if (httpURLConnection != null) {
+				httpURLConnection.disconnect();
+			}
+		}
+		return null;
+	}
+}
