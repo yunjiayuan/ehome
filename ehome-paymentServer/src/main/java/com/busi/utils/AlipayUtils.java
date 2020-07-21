6a4768e82db870fd435fc566b0b36baaeaee36cc
suntj <552440192@qq.com>
2020-07-21 15:04:09 +0800

    1、修改提现到支付宝相关业务逻辑

--- ehome-paymentServer/src/main/java/com/busi/utils/AlipayUtils.java
+++ ehome-paymentServer/src/main/java/com/busi/utils/AlipayUtils.java
@@ -87,29 +87,30 @@ public class AlipayUtils {
      * 给客户端使用
      */
     public static String getLoginSign(){
-        //请求参数数据格式(仅json)
-        String format = "json";
-        //请求使用的编码格式,如utf-8,gbk,gb2312等
-        String charset = "utf-8";
-        //商户生成签名字符串所使用的签名算法类型，目前支持RSA2和RSA，推荐使用RSA2
-        String signType = "RSA2";
-        CertAlipayRequest certAlipayRequest = new CertAlipayRequest();
-        certAlipayRequest.setServerUrl(Constants.ALIPAY_URL);
-        certAlipayRequest.setAppId(Constants.ALIPAY_APPID);
-        certAlipayRequest.setPrivateKey(Constants.ALIPAY_PRIVATE_KEY);
-        certAlipayRequest.setFormat(format);
-        certAlipayRequest.setCharset(charset);
-        certAlipayRequest.setSignType(signType);
-        //证书路径必须是绝对路径
-        //应用公钥证书绝对路径
-        certAlipayRequest.setCertPath(Constants.ALIPAY_PUBLIC_KEY);
-        //支付宝公钥证书绝对路径
-        certAlipayRequest.setAlipayPublicCertPath(Constants.ALIPAY_ALIPAYCERTPUBLICKEY_KEY);
-        //支付宝根证书绝对路径
-        certAlipayRequest.setRootCertPath(Constants.ALIPAY_ALIPAYROOTCERT_KEY);
-        AlipayClient alipayClient = null;
+//        //请求参数数据格式(仅json)
+//        String format = "json";
+//        //请求使用的编码格式,如utf-8,gbk,gb2312等
+//        String charset = "utf-8";
+//        //商户生成签名字符串所使用的签名算法类型，目前支持RSA2和RSA，推荐使用RSA2
+//        String signType = "RSA2";
+//        CertAlipayRequest certAlipayRequest = new CertAlipayRequest();
+//        certAlipayRequest.setServerUrl(Constants.ALIPAY_URL);
+//        certAlipayRequest.setAppId(Constants.ALIPAY_APPID);
+//        certAlipayRequest.setPrivateKey(Constants.ALIPAY_PRIVATE_KEY);
+//        certAlipayRequest.setFormat(format);
+//        certAlipayRequest.setCharset(charset);
+//        certAlipayRequest.setSignType(signType);
+//        //证书路径必须是绝对路径
+//        //应用公钥证书绝对路径
+//        certAlipayRequest.setCertPath(Constants.ALIPAY_PUBLIC_KEY);
+//        //支付宝公钥证书绝对路径
+//        certAlipayRequest.setAlipayPublicCertPath(Constants.ALIPAY_ALIPAYCERTPUBLICKEY_KEY);
+//        //支付宝根证书绝对路径
+//        certAlipayRequest.setRootCertPath(Constants.ALIPAY_ALIPAYROOTCERT_KEY);
+//        AlipayClient alipayClient = null;
         try {
-            alipayClient = new DefaultAlipayClient(certAlipayRequest);
+            AlipayClient alipayClient = new DefaultAlipayClient(Constants.ALIPAY_URL,Constants.ALIPAY_APPID,Constants.ALIPAY_PRIVATE_KEY,"json","GBK",Constants.ALIPAY_PUBLIC_KEY,"RSA2");
+//            alipayClient = new DefaultAlipayClient(certAlipayRequest);
             AlipayUserInfoAuthRequest request = new AlipayUserInfoAuthRequest();
             request.setBizContent("{" +
                     "      \"scopes\":[" +
@@ -118,7 +119,7 @@ public class AlipayUtils {
                     "\"state\":\"init\"" +
                     "  }");
             AlipayUserInfoAuthResponse response = null;
-            response = alipayClient.certificateExecute(request);
+            response = alipayClient.pageExecute(request);
             if(response.isSuccess()){
                 String sign = "";
 //                sign = response.getBody().substring(response.getBody().indexOf("&sign=")+6,response.getBody().indexOf("&version="));
