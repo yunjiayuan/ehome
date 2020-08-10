e95d4c7a9bdcb9d8bb53e34288460320b050e818
zhjj <tony.jj@foxmail.com>
2020-08-10 17:18:22 +0800

    1.新增家门口买药相关关系对照 2.新增家门口药店相关接口 3.新增家门口药店药品相关接口 4.新增家门口药店收藏相关接口

---
+++ ehome-api/otherServer-api/src/main/java/com/busi/entity/PharmacyCollection.java
@@ -0,0 +1,32 @@
+package com.busi.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Min;
+import java.util.Date;
+
+/**
+ * @program: 药房收藏
+ * @author: ZHaoJiaJie
+ * @create: 2020-08-10 14:17:16
+ */
+@Setter
+@Getter
+public class PharmacyCollection {
+
+    private long id;        //主键ID
+
+    @Min(value = 1, message = "myId参数有误")
+    private long myId;        //用户ID
+
+    private long userId;                //创建者ID
+
+    private String name;                //药房名字
+
+    private String picture;        //图片
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date time;        //收藏时间
+}
