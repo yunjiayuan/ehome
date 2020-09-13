48505764a89fcabb1ee00f6e3801422c32c23ac4
suntj <552440192@qq.com>
2020-09-13 17:57:53 +0800

    1、修改提现定时任务

--- ehome-quartz-paymentServer/src/main/java/com/busi/timerController/CashOutTimerController.java
+++ ehome-quartz-paymentServer/src/main/java/com/busi/timerController/CashOutTimerController.java
@@ -3,6 +3,9 @@ package com.busi.timerController;
 import com.busi.Feign.CashOutLControllerFegin;
 import com.busi.entity.CashOutOrder;
 import com.busi.servive.CashOutService;
+import com.busi.utils.CommonUtils;
+import com.busi.utils.Constants;
+import com.busi.utils.RedisUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -25,6 +28,9 @@ public class CashOutTimerController {
     private CashOutService cashOutService;
 
     @Autowired
+    private RedisUtils redisUtils;
+
+    @Autowired
     private CashOutLControllerFegin cashOutLControllerFegin;
 
     @Scheduled(cron = "0 33 1 * * ?") // 每天晚上1点33执行一次
@@ -38,6 +44,8 @@ public class CashOutTimerController {
                 r = (CashOutOrder) list.get(i);
                 if (r != null) {
                     //调用paymentServer服务中的 更新钱包余额操作
+                    //将订单放入缓存中  15分钟有效时间  超时作废
+                    redisUtils.hmset(Constants.REDIS_KEY_PAY_ORDER_CASHOUT+r.getId(),CommonUtils.objectToMap(r),Constants.TIME_OUT_MINUTE_15);
                     cashOutLControllerFegin.cashOutToOther(r);
                     log.info("消息服务平台处理用户[" + r.getUserId() + "]提现操作成功！");
                     arrList.add(r);
