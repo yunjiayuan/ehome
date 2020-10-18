c6525b7ab3565084b508dd8911ba76709fa54b45
suntj <552440192@qq.com>
2020-10-18 19:56:12 +0800

    1、添加管理员权限查看下奖励系统总金额

--- ehome-otherServer/src/main/java/com/busi/controller/api/RewardTotalMoneyLogController.java
+++ ehome-otherServer/src/main/java/com/busi/controller/api/RewardTotalMoneyLogController.java
@@ -49,8 +49,10 @@ public class RewardTotalMoneyLogController extends BaseController implements Rew
     public ReturnData findTotalRewardMoney(@PathVariable long userId) {
         //验证权限
         long myId = CommonUtils.getMyId();
-        if (myId != userId) {
-            return returnData(StatusCode.CODE_PARAMETER_ERROR.CODE_VALUE, "参数有误，当前用户[" + CommonUtils.getMyId() + "]无权限操作用户[" + userId + "]的奖励总金额信息", new JSONObject());
+        if(CommonUtils.getAdministrator(myId,redisUtils)<1){
+            if (myId != userId) {
+                return returnData(StatusCode.CODE_PARAMETER_ERROR.CODE_VALUE, "参数有误，当前用户[" + CommonUtils.getMyId() + "]无权限操作用户[" + userId + "]的奖励总金额信息", new JSONObject());
+            }
         }
         Map<String, Object> rewardTotalMoneyLogMap = redisUtils.hmget(Constants.REDIS_KEY_REWARD_TOTAL_MONEY + userId);
         if (rewardTotalMoneyLogMap == null || rewardTotalMoneyLogMap.size() <= 0) {
