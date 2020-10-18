0588c5ab0e7a9311bb2e9124c204384ac9c1ebe0
suntj <552440192@qq.com>
2020-10-18 11:42:04 +0800

    1、暂时去掉查询时判断证照为空的逻辑

--- ehome-otherServer/src/main/java/com/busi/dao/HotelTourismDao.java
+++ ehome-otherServer/src/main/java/com/busi/dao/HotelTourismDao.java
@@ -205,7 +205,7 @@ public interface HotelTourismDao {
     @Select("<script>" +
             "select *, ROUND(6378.138*2*ASIN(SQRT(POW(SIN((#{lat}*PI()/180-lat*PI()/180)/2),2)+COS(#{lat}*PI()/180)*COS(lat*PI()/180)*POW(SIN((#{lon}*PI()/180-lon*PI()/180)/2),2)))*1000) AS juli " +
             " from KitchenReserve" +
-            " where deleteType = 0 and healthyCard != '' " +
+            " where deleteType = 0 " +
             " and auditType = #{auditType}" +
             " order by juli asc" +
             "</script>")
