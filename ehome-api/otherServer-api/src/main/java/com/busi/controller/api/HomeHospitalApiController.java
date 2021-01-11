88d5d47a5b542a67eba8ddca6c265353704d4769
zhjj <tony.jj@foxmail.com>
2021-01-11 14:18:32 +0800

    1.完善活动审核相关接口 2.完善足迹查询接口 3.家医生查询接口新增经纬度参数，返回数据新增距离

--- ehome-api/otherServer-api/src/main/java/com/busi/controller/api/HomeHospitalApiController.java
+++ ehome-api/otherServer-api/src/main/java/com/busi/controller/api/HomeHospitalApiController.java
@@ -56,18 +56,21 @@ public interface HomeHospitalApiController {
 
     /***
      * 查询列表
+     * @param watchVideos 筛选视频：0否 1是
      * @param cityId     默认-1 百度地图中的城市ID，用于同城搜索
      * @param department      科室
      * @param search    模糊搜索（可以是：症状、疾病、医院、科室、医生名字）
      * @param province     省
      * @param city      市
      * @param district    区
+     * @param lat      纬度
+     * @param lon      经度
      * @param page     页码
      * @param count    条数
      * @return
      */
-    @GetMapping("findHospitalList/{cityId}/{watchVideos}/{department}/{search}/{province}/{city}/{district}/{page}/{count}")
-    ReturnData findHospitalList(@PathVariable int cityId, @PathVariable int watchVideos, @PathVariable int department, @PathVariable String search, @PathVariable int province, @PathVariable int city, @PathVariable int district, @PathVariable int page, @PathVariable int count);
+    @GetMapping("findHospitalList/{cityId}/{watchVideos}/{department}/{search}/{province}/{city}/{district}/{lat}/{lon}/{page}/{count}")
+    ReturnData findHospitalList(@PathVariable int cityId, @PathVariable int watchVideos, @PathVariable int department, @PathVariable String search, @PathVariable int province, @PathVariable int city, @PathVariable int district, @PathVariable double lat, @PathVariable double lon, @PathVariable int page, @PathVariable int count);
 
     /**
      * @Description: 删除
